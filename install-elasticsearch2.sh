c0aa36b337911a9f219e2d1c1997840256e55c26
gomesuit <gomesuit>
2018-06-19 18:41:17 +0900

    save

--- install-elasticsearch2.sh
+++ install-elasticsearch2.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 yum -y install java-1.8.0-openjdk
-cp -f /vagrant/elasticsearch.repo /etc/yum.repos.d/elasticsearch.repo
+cp -f /vagrant/etc/yum.repos.d/elasticsearch.repo /etc/yum.repos.d/elasticsearch.repo
 yum -y install elasticsearch
-systemctl start elasticsearch
+
+cp -f /etc/elasticsearch/jvm.options /etc/elasticsearch/jvm.options.bk
+cp -f /vagrant/etc/elasticsearch/jvm.options /etc/elasticsearch/jvm.options
+
 systemctl enable elasticsearch
+systemctl start elasticsearch
