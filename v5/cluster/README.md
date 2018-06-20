c4b4c582b41c8928acdbc83ac96aba020fdf74e3
gomesuit <gomesuit>
2018-06-20 18:06:36 +0900

    save

--- v5/cluster/README.md
+++ v5/cluster/README.md
@@ -2,3 +2,8 @@
 
 - 参考
   - https://qiita.com/Esfahan/items/3c07bfbb57c7098e9531
+  - https://ishiis.net/2016/09/19/elasticsearch-vagrant/
+
+```bash
+curl http://192.168.33.11:9200/_cluster/health?pretty
+```
--- v5/cluster/install-elasticsearch.sh
+++ v5/cluster/install-elasticsearch.sh
@@ -7,9 +7,6 @@ yum -y install elasticsearch
 cp -f /etc/elasticsearch/jvm.options /etc/elasticsearch/jvm.options.bk
 cp -f /vagrant/etc/elasticsearch/jvm.options /etc/elasticsearch/jvm.options
 
-cp -f /etc/elasticsearch/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml.bk
-cp -f /vagrant/etc/elasticsearch/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
-
 /usr/share/elasticsearch/bin/elasticsearch-plugin install analysis-kuromoji
 
 systemctl enable elasticsearch
