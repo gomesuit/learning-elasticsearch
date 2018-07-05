99ad18fc4ee208db2cf48b712d191fc40d5ad561
gomesuit <gomesuit>
2018-07-05 15:00:51 +0900

    save

--- v5/standard/install-elasticsearch.sh
+++ v5/standard/install-elasticsearch.sh
@@ -3,6 +3,7 @@
 yum -y install java-1.8.0-openjdk
 cp -f /vagrant/etc/yum.repos.d/elasticsearch.repo /etc/yum.repos.d/elasticsearch.repo
 yum -y install elasticsearch-5.1.1-1
+yum -y install kibana-5.1.1-1
 
 cp -f /etc/elasticsearch/jvm.options /etc/elasticsearch/jvm.options.bk
 cp -f /vagrant/etc/elasticsearch/jvm.options /etc/elasticsearch/jvm.options
@@ -12,5 +13,14 @@ cp -f /vagrant/etc/elasticsearch/elasticsearch.yml /etc/elasticsearch/elasticsea
 
 /usr/share/elasticsearch/bin/elasticsearch-plugin install analysis-kuromoji
 
+/usr/share/elasticsearch/bin/elasticsearch-plugin install x-pack
+
+yum -y install kibana-5.1.1-1
+cp -f /vagrant/etc/kibana/kibana.yml /etc/kibana/kibana.yml
+/usr/share/kibana/bin/kibana-plugin install x-pack
+
 systemctl enable elasticsearch
 systemctl start elasticsearch
+
+systemctl enable kibana
+systemctl start kibana
